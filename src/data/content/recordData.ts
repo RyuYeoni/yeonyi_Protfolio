06a8a7a3fb6887bcc3e61b10aab6701e1d7e31b7
ryu yeoni <yeoni0915@naver.com>
2025-09-20 17:13:05 +0900

    docs: update record

--- src/data/content/recordData.ts
+++ src/data/content/recordData.ts
@@ -1,21 +1,18 @@
 export const recordData = [
   {
-    data: '2023.02 - 2023.08',
-    title: '코드스테이츠',
-    role: '@ 프론트엔드 엔지니어 과정 부트캠프 수료',
+    data: '2022.03 - Present',
+    title: 'Gachon Univerisity, South Korea',
+    role: '@ Software Departemt',
     infos: [
-      'JavaScript, React 등 학습',
-      '20주 동안 매일 강도 높은 과제 수행 및 페어 프로그래밍과 코드 리뷰 경험',
-      '솔로 프로젝트 1회, 협업 프로젝트 2회 진행',
-      '스터디에 참여하여 개발 공부를 진행하고 동료들과 원활한 소통과 커뮤니케이션을 촉진',
+      'Studied computer science subjects such as data structures, operating systems, and algorithms.', 'Learned programming languages including C, Java, Python, and Kotlin.', 'Improved communication skills through team projects every semester.'
     ],
-    stacks: ['html', 'css', 'javascript', 'react', 'github', 'figma'],
+    stacks: ['c', 'java', 'python', 'html', 'css', 'javascript', 'kotlin','sql', 'github', 'figma'],
   },
   {
-    data: '2015.03 - 2017.02',
-    title: '전주비전대학교',
-    role: '@ 컴퓨터정보과 전문학사 졸업',
-    infos: ['자료구조, 운영체제 등 CS 학습', 'HTML, CSS, JavaScript 등 프로그래밍 언어 학습'],
-    stacks: ['html', 'css', 'ps', 'ai'],
+    data: '2024.04 - 2024.08',
+    title: 'Johannes Gutenberg University Mainz, Germany',
+    role: '@ Computer Science',
+    infos: ['Learning AI course'],
+    stacks: ['python'],
   },
 ];
